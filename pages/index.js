e81c05e4c36d547ba6fb0e9fa6ce1beee330fa0a
Camille <camille.coutens@gmail.com>
2021-02-02 22:24:34 +0100

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Ta vérité n'est pas la mienne.</h1>
+    </div>
+  )
+}
