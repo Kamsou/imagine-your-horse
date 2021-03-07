02e42a72d0fe7b5695aa981af396d3d396c1cb22
Camille <camille.coutens@gmail.com>
2021-03-07 12:13:08 +0100

    ADD: test API

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,38 @@
+import Image from 'next/image'
 import Layout from '../src/components/layout'
 
-export default function Home() {
+const defaultEndpoint = 'https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations'
+
+export async function getServerSideProps() {
+  const res = await fetch(defaultEndpoint)
+  const data = await res.json()
+  return {
+    props: {
+      data
+    }
+  }
+}
+
+export default function Home({ data }) {
+  const results = data.civilizations
+  console.log(results);
   return (
-    <Layout>
-      <div class="flex justify-center items-center h-screen">
-        <h1>Bonswar</h1>
+    <Layout className="relative">
+      <div className="w-full h-5/6 absolute p-5">
+        <Image src="/images/horse.png" alt="Emoji Poney" width="100px" height="100px"/>
+        <div className="list-none font-bold text-lg">
+          {
+            results.map(result => {
+              const { id, name } = result
+
+              return (
+                <li key={id}>
+                  <h3>{ name }</h3>
+                </li>
+              )
+            })
+          }
+        </div>
       </div>
     </Layout>
   )
--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -2,9 +2,33 @@ import Header from '../header';
 
 function Layout({children}) {
   return (
-    <div>
+    <div className="h-screen">
       <Header />
       {children}
+      <link
+        rel="preload"
+        href="/fonts/AtkinsonHyperlegible/Atkinson-Hyperlegible-Regular.woff"
+        as="font"
+        crossOrigin=""
+      />
+      <link
+        rel="preload"
+        href="/fonts/AtkinsonHyperlegible/Atkinson-Hyperlegible-Regular.woff2"
+        as="font"
+        crossOrigin=""
+      />
+      <link
+        rel="preload"
+        href="/fonts/AtkinsonHyperlegible/Atkinson-Hyperlegible-Bold.woff"
+        as="font"
+        crossOrigin=""
+      />
+      <link
+        rel="preload"
+        href="/fonts/AtkinsonHyperlegible/Atkinson-Hyperlegible-Bold.woff2"
+        as="font"
+        crossOrigin=""
+      />
     </div>
   )
 }
